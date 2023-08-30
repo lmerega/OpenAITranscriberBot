8c87dea278889c4fc16b2d1ed29789a2de6be17c
lmerega <luca@lucamerega.it>
2023-08-30 22:50:57 +0200

    Code completely revised to allow public use!

--- README.md
+++ README.md
@@ -1,69 +1,119 @@
 # Telegram Audio Transcription Bot
 
 ## Description
-This project is a Telegram bot built using the `telebot` Python library and OpenAI's GPT-3.5-turbo and Whisper-1 models. It is designed to receive voice messages, transcribe them, process the transcription with the GPT-3.5-turbo model, and send the result back to the user. It supports multiple languages for system prompts and error messages.
+
+Introducing the Telegram Audio Transcription Bot: a cutting-edge solution that uses the `telebot` Python library in tandem with OpenAI's GPT-3.5-turbo and Whisper-1 models. It's designed to transcribe voice messages, refine these transcriptions using the GPT-3.5-turbo model, and then send back polished results to the user. The bot's multi-language support is exemplary; it automatically detects user language preferences and delivers translated system prompts and error messages for an enhanced user experience. Prioritizing user data security, the bot encrypts individual OpenAI API keys in the database.
+
+**Live Version:** Experience the bot's capabilities first-hand at [https://t.me/lmewhisperbot](https://t.me/lmewhisperbot).
+
+## Changelog
+
+- **Version 2.0.0**:
+  - Integrated multi-language support with automatic language detection.
+  - Introduced new commands: `/changelanguage` allowing users to switch between supported languages and `/changekey` to update OpenAI API keys.
+  - Enhanced data security with encryption of OpenAI API keys within the database.
+  - Revamped system prompts and error messages for intuitive user interactions.
+  - Strengthened error handling and integrated detailed logging for streamlined troubleshooting.
+  - Optimized overall bot performance.
 
 ## Requirements
-* Python 3.6+
-* telebot library
-* requests library
-* openai library
-* ffmpeg for audio conversion
+
+- Python 3.6+
+- telebot library
+- requests library
+- openai library
+- cryptography library
+- mysql-connector library
+- ffmpeg for audio conversion
 
 ## Installation
 
 ```sh
-pip install pyTelegramBotAPI requests openai
+pip install pyTelegramBotAPI requests openai cryptography mysql-connector-python
 ```
 
-*Note: ffmpeg needs to be installed separately, please refer to the official ffmpeg [installation guide](https://ffmpeg.org/download.html).*
+*Note: ffmpeg must be installed separately. Please follow the official ffmpeg [installation guide](https://ffmpeg.org/download.html).*
 
 ## Setup
 
-1. Obtain an API key from OpenAI at their [developer site](https://beta.openai.com/signup/).
-2. Generate a Telegram Bot token following Telegram's [BotFather guide](https://core.telegram.org/bots#6-botfather).
-3. Clone this repository and create a file named `config.json` in the root directory. The structure of `config.json` should be:
+1. Secure an API key from OpenAI on their [developer site](https://beta.openai.com/signup/).
+2. Generate a Telegram Bot token using Telegram's [BotFather guide](https://core.telegram.org/bots#6-botfather).
+3. Clone this repository.
+4. Configure MySQL and construct the needed database and tables. The **Database Setup** section provides thorough details.
+5. In the root directory, construct a `config.json` following the structure below:
+
 ```json
 {
+    "encryption_key": "YOUR_GENERATED_ENCRYPTION_KEY",
     "openai_api_key": "your_openai_api_key",
-    "bot_token": "your_telegram_bot_token"
+    "bot_token": "your_telegram_bot_token",
+    "db_config": {
+        "host": "YOUR_DB_HOST",
+        "user": "YOUR_DB_USER",
+        "password": "YOUR_DB_PASSWORD",
+        "database": "YOUR_DB_NAME"
+    }
 }
 ```
-4. To add or modify supported languages, you can modify the `languages.json` file in the root directory. The structure of `languages.json` is as follows:
-```json
-{
-    "it": {
-        "system_prompt": "Italian_system_prompt",
-        "welcome_message": "Italian_welcome_message",
-        "recognition_started": "Italian_recognition_started_message",
-        "transcription_error": "Italian_transcription_error_message",
-        "bot_connected_message": "Italian_bot_connected_message",
-        "bot_connection_error": "Italian_bot_connection_error_message"
-    },
-    "en": {...}
-}
+
+*Tip: For generating an `encryption_key`, utilize the `cryptography` library in Python. Details are available in the **Generating an `encryption_key`** section.*
+
+6. Add or modify supported languages via the `languages.json` in the root directory.
+
+## Database Setup
+
+1. Activate the MySQL server.
+2. Implement the SQL commands below to set up the necessary database and table:
+
+```sql
+CREATE DATABASE TranscriptionBotDB;
+
+USE TranscriptionBotDB;
+
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    openai_api_key_encrypted BLOB NOT NULL,
+    preferred_language VARCHAR(5) NOT NULL DEFAULT 'en'
+);
 ```
-5. Replace `language = "it"` in the code with the language code you want to use for system messages.
+
+Note: The `preferred_language` defaults to 'en' (English). Users can adjust this setting through the `/changelanguage` command.
 
 ## Usage
-1. Start your bot by running `python main.py` (or your script file's name).
-2. In the Telegram app, search for your bot's username and start a chat with it.
-3. The bot accepts `/start` and `/help` commands, which will display a welcome message. It also responds to voice messages, transcribing and processing them.
+
+1. Start the bot with the command: `python main.py`.
+2. On Telegram, find your bot using its username to begin a chat.
+3. The bot recognizes the commands: `/start`, `/help`, `/changekey`, and `/changelanguage`:
+   - `/start` & `/help`: Presents a warm welcome and essential instructions.
+   - `/changekey`: Allows users to update their OpenAI API key.
+   - `/changelanguage`: Enables users to select from a list of supported languages.
+
+4. The bot is also proficient at managing voice messages, ensuring they are transcribed and enhanced using the OpenAI models.
 
 ## Functions
 
-- `remove_phrases(text)`: This function removes specific phrases from the text (defined in the `phrases` list).
+- `remove_phrases(text)`: Omit specific phrases from text.
+- `generate_corrected_transcript(temperature, system_prompt, audio_file)`: Refines transcribed content through the GPT-3.5-turbo model.
+- `send_welcome(message)`: Responds to the `/start` and `/help` commands.
+- `get_voice_message(message)`: Manages voice messages, transcribing and processing them.
+- `change_api_key_command(message)`: Enables users to modify their OpenAI API key stored in the database.
 
-- `generate_corrected_transcript(temperature, system_prompt, audio_file)`: This function uses OpenAI's GPT-3.5-turbo model to process the transcribed text.
+## Generating an `encryption_key`
 
-- `send_welcome(message)`: This function handles the `/start` and `/help` commands.
+For ensuring the utmost security of OpenAI API keys in the database, make use of Fernet symmetric encryption:
 
-- `get_voice_message(message)`: This function handles voice messages. It downloads the audio, transcribes it, processes the transcription with GPT-3.5-turbo, and sends back the result.
+```python
+from cryptography.fernet import Fernet
+key = Fernet.generate_key()
+print(key.decode())
+```
+
+Incorporate this generated key as the `encryption_key` in your `config.json`.
 
 ## Disclaimer
 
-This project uses OpenAI's GPT-3.5-turbo and Whisper-1 models, which may incur costs. Please refer to OpenAI's [pricing](https://openai.com/pricing) for more information.
+The bot operates using OpenAI's GPT-3.5-turbo and Whisper-1 models. It's important to note that usage may lead to associated costs. Always check OpenAI's [pricing](https://openai.com/pricing) for detailed information.
 
 ## License
 
-This project is licensed under the terms of the MIT license.
+This project adheres to the terms of the MIT license.
--- transcriberBot.py
+++ transcriberBot.py
@@ -5,32 +5,112 @@ import subprocess
 import os
 import uuid
 import json
+import mysql.connector
+import logging
 
-with open('config.json') as config_file:
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler(),
+                              logging.FileHandler('app.log', encoding='utf-8')])
+
+language = None
+current_openai_key = None
+
+languages = {
+    "English": "en",
+    "Italiano": "it",
+    "Français": "fr",
+    "Español": "es",
+    "Deutsch": "de"
+}
+
+logger = logging.getLogger(__name__)
+from cryptography.fernet import Fernet
+
+with open('config.json', 'r') as config_file:
     config = json.load(config_file)
+    cipher = Fernet(config['encryption_key'])
+    db_config = config['db_config']
+
 
 with open('languages.json') as lang_file:
     lang_resources = json.load(lang_file)    
 
-openai.api_key = config['openai_api_key']
+#openai.api_key = config['openai_api_key']
 bot_token = config['bot_token'] 
-
-phrases = lang_resources['phrases']
-language = "it"
-system_prompt = lang_resources[language]['system_prompt']
-welcome_message = lang_resources[language]['welcome_message']
-recognition_started = lang_resources[language]['recognition_started']
-transcription_error = lang_resources[language]['transcription_error']
-bot_connected_message = lang_resources[language]['bot_connected_message'] 
-bot_connection_error = lang_resources[language]['bot_connection_error']
-
 bot = telebot.TeleBot(bot_token)
 
 try:
     bot_info = bot.get_me()
-    print(bot_connected_message, bot_info.id)
+    logger.debug("%s %s", "Bot connected with id: ", bot_info.id)
 except Exception as e:
-    print(bot_connection_error, e)
+    logger.error("%s %s", "Bot connection error: ", e)
+
+def encrypt_data(data):
+    encrypted_data = cipher.encrypt(data.encode())
+    return encrypted_data
+
+def decrypt_data(encrypted_data):
+    decrypted_data = cipher.decrypt(encrypted_data).decode()
+    return decrypted_data
+
+def has_valid_api_key(chat_id):
+    if current_openai_key:
+        return is_valid_openai_key(current_openai_key)
+    api_key = get_api_key_from_db(chat_id)
+    return is_valid_openai_key(api_key)
+
+def get_api_key_from_db(chat_id):
+    global current_openai_key
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+    cursor.execute("SELECT encrypted_openai_api_key FROM users WHERE chat_id = %s", (chat_id,))
+    encrypted_api_key = cursor.fetchone()
+    cursor.close()
+    cnx.close()
+
+    if encrypted_api_key is None or not encrypted_api_key[0]:
+        return None
+
+    decrypted_key = decrypt_data(encrypted_api_key[0])
+    current_openai_key = decrypted_key 
+    return decrypted_key
+
+def store_api_key_in_db(chat_id, api_key):
+    encrypted_api_key = encrypt_data(api_key)
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+    cursor.execute("INSERT INTO users (chat_id, encrypted_openai_api_key) VALUES (%s, %s) ON DUPLICATE KEY UPDATE encrypted_openai_api_key = %s", 
+                   (chat_id, encrypted_api_key, encrypted_api_key))
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+def store_provided_key(message):
+    chat_id = message.chat.id
+    bot.clear_step_handler_by_chat_id(chat_id=chat_id)  
+
+    api_key = message.text
+    if message.text == "NO":
+        bot.clear_step_handler_by_chat_id(chat_id=chat_id)
+        bot.send_message(chat_id, operation_cancelled)
+        return    
+    if is_valid_openai_key(api_key):
+        store_api_key_in_db(chat_id, api_key)
+        bot.reply_to(message, key_stored)
+    else:
+        bot.reply_to(message, invalid_api_key)
+        bot.register_next_step_handler(message, store_provided_key)  
+
+def is_valid_openai_key(api_key):
+    openai.api_key = api_key
+    try:
+        models = openai.Model.list()
+        if models:  
+            return True
+    except Exception as e:
+        logger.error( "%s %s", "Error during OpenAI API call:", e)
+        return False 
 
 def remove_phrases(text):
     for phrase in phrases:
@@ -54,25 +134,154 @@ def generate_corrected_transcript(temperature, system_prompt, audio_file):
     )
     return response['choices'][0]['message']['content']
 
-@bot.message_handler(commands=["help", "start"])
-def send_welcome(message):
+def change_api_key_step(message):
+    new_api_key = message.text
+    chat_id = message.chat.id
+    if message.text == "NO":
+        bot.clear_step_handler_by_chat_id(chat_id=chat_id)
+        bot.send_message(chat_id, operation_cancelled)
+        return    
+    if is_valid_openai_key(new_api_key):
+        store_api_key_in_db(chat_id, new_api_key)
+        bot.send_message(message.chat.id, key_updated)
+    else:
+        bot.send_message(message.chat.id, invalid_api_key)
+        bot.register_next_step_handler(message, change_api_key_step)
+
+def get_language_from_db(chat_id):
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+    cursor.execute("SELECT language FROM users WHERE chat_id = %s", (chat_id,))
+    lang = cursor.fetchone()
+    cursor.close()
+    cnx.close()
+    return lang[0] if lang else "en"  
+
+def store_language_in_db(chat_id, lang):
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+    cursor.execute("INSERT INTO users (chat_id, language) VALUES (%s, %s) ON DUPLICATE KEY UPDATE language = %s", 
+                   (chat_id, lang, lang))
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+def load_language_resources(lang):
+    global phrases, system_prompt, welcome_message, help_message, recognition_started, transcription_error
+    global bot_connected_message, bot_connection_error, provide_api_key, invalid_api_key, key_stored
+    global key_updated, operation_cancelled, language_choice, language_error, select_lang
+
+    with open('languages.json', 'r') as lang_file:
+        lang_resources = json.load(lang_file)
+        phrases = lang_resources['phrases']
+        system_prompt = lang_resources[lang]['system_prompt']
+        welcome_message = lang_resources[lang]['welcome_message']
+        help_message = lang_resources[lang]['help_message']
+        recognition_started = lang_resources[lang]['recognition_started']
+        transcription_error = lang_resources[lang]['transcription_error']
+        provide_api_key = lang_resources[lang]['provide_api_key']
+        invalid_api_key = lang_resources[lang]['invalid_api_key']
+        key_stored = lang_resources[lang]['key_stored']
+        key_updated = lang_resources[lang]['key_updated']
+        operation_cancelled= lang_resources[lang]['operation_cancelled']
+        language_choice = lang_resources[lang]['language_choice']
+        language_error = lang_resources[lang]['language_error']
+        select_lang = lang_resources[lang]['select_lang']
+
+def set_language(message):
+    global language
+    chat_id = message.chat.id
+    lang_choice = message.text
+
+    if lang_choice in languages:
+        language = languages[lang_choice]
+        store_language_in_db(chat_id, language)
+        load_language_resources(language)
+        bot.send_message(chat_id, f"{language_choice} {lang_choice}!")
+
+        markup = telebot.types.ReplyKeyboardRemove()
+
+    else:
+        bot.send_message(chat_id, language_error)
+        send_language_keyboard(chat_id)
+
+def send_language_keyboard(chat_id):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    for lang in languages:
+        markup.add(lang)
+    
+    bot.send_message(chat_id, select_lang, reply_markup=markup)
+    bot.register_next_step_handler_by_chat_id(chat_id, set_language)
+
+@bot.message_handler(commands=["help"])
+def send_help(message):
+    global language
+    chat_id = message.chat.id
+    if not language:
+        language = get_language_from_db(chat_id)
+    load_language_resources(language)    
     bot.reply_to(
-        message, welcome_message,
+        message, help_message,
     )
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    global language
+    chat_id = message.chat.id
+    if not language:
+        language = get_language_from_db(chat_id)
+    load_language_resources(language)
+    bot.reply_to(message, welcome_message)
+
 @bot.message_handler(commands=["stop_bot"])
 def stop_bot_handler(message):
     bot.stop_bot()
 
+@bot.message_handler(commands=["changekey"])
+def change_api_key_command(message):
+    global language
+    chat_id = message.chat.id
+    if not language:
+        language = get_language_from_db(chat_id)
+    load_language_resources(language)    
+    bot.send_message(message.chat.id, provide_api_key)
+    bot.register_next_step_handler(message, change_api_key_step)
+
+@bot.message_handler(commands=["changelanguage"])
+def change_language_command(message):
+    global language
+    chat_id = message.chat.id
+    if not language:
+        language = get_language_from_db(chat_id)
+    load_language_resources(language)    
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    for lang_name in languages:
+        markup.add(telebot.types.KeyboardButton(lang_name))
+    bot.send_message(message.chat.id, select_lang, reply_markup=markup)
+    bot.register_next_step_handler(message, set_language)
+
+
 @bot.message_handler(content_types=['voice'])
 def get_voice_message(message):
-    print('Received audio file')
-
+    global language
+    chat_id = message.chat.id
+    if not language:
+        language = get_language_from_db(chat_id)
+    load_language_resources(language) 
+    if current_openai_key is None:
+        api_key = get_api_key_from_db(chat_id)
+        if not has_valid_api_key(chat_id):
+            bot.reply_to(message, provide_api_key)
+            bot.register_next_step_handler(message, store_provided_key)
+            return
+    else:
+        api_key = current_openai_key
+        
     file_info = bot.get_file(message.voice.file_id)
     file_path = file_info.file_path
-
+    openai.api_key = api_key
     file_url = f'https://api.telegram.org/file/bot{bot_token}/{file_path}'
 
-    print('Downloading audio file...')
+    logger.info('Downloading audio file...')
     audio_file = requests.get(file_url)
 
     file_extension = file_path.split('.')[-1]
@@ -81,7 +290,7 @@ def get_voice_message(message):
         f.write(audio_file.content)
 
     sent_message = bot.reply_to(message, recognition_started, parse_mode='Markdown')
-    print('Converting audio file in MP3 format...')
+    logger.info('Converting audio file in MP3 format...')
     mp3_file_name = f'{str(uuid.uuid4())}.mp3'
     subprocess.run(['ffmpeg', '-i', file_name, mp3_file_name])
 
@@ -93,24 +302,24 @@ def get_voice_message(message):
                 model="whisper-1",
                 response_format="text"
             )
-            print(transcript)
+            logger.debug(transcript)
             transcript = remove_phrases(transcript)
-            print(transcript)
+            logger.debug(transcript)
             if transcript is not None and transcript.strip():
                 corrected_text = generate_corrected_transcript(0, system_prompt, transcript)
-                print(corrected_text)
+                logger.debug(corrected_text)
                 bot.edit_message_text(chat_id=message.chat.id, message_id=sent_message.message_id, text=corrected_text)
             else:
                 bot.edit_message_text(chat_id=message.chat.id, message_id=sent_message.message_id, text=transcription_error)
     except Exception as e:
-        print("Error transcribing audio file:", e)
+        logger.error("%s %s", "Error transcribing audio file:", e)
         bot.edit_message_text(chat_id=message.chat.id, message_id=sent_message.message_id, text=transcription_error)
 
-    print('Deleting audio file...')
+    logger.info('Deleting audio file...')
     os.remove(file_name)
     os.remove(mp3_file_name)
 
-    print('Original file name:', file_name)
-    print('Converted file name:', mp3_file_name)
+    logger.info("%s %s", 'Original file name:', file_name)
+    logger.info("%s %s", 'Converted file name:', mp3_file_name)
 
 bot.polling()
